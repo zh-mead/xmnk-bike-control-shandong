407bb67d8d54ded961cc2092ffec1879dacf0f00
mead <751066209@qq.com>
2023-05-05 19:44:17 +0800

    init

---
+++ src/Common/Maps/PropertyMap.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ZhMead\XmnkBikeControl\Common\Maps;
+class PropertyMap
+{
+    const isSync = 'isSync';
+    const userRoleTag = 'userRoleTag';
+    const isDev = 'isDev';
+    const isAutoBikeStatusSync = 'isAutoBikeStatusSync';
+}
