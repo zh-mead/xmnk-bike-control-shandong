128fb2e7b6d7b80454e4f182bbe77325729f2f6b
mead <751066209@qq.com>
2023-09-07 14:17:23 +0800

    init

--- src/Tbit/Control.php
+++ src/Tbit/Control.php
@@ -204,7 +204,7 @@ class Control implements ControlInterface
      */
     public function openHelmet($box_no, $isSync = -1)
     {
-        $msg_id = $this->makeMsgId($box_no, $this->userRoleTag, CmdMap::CONTROL_REMOTE_OPEN_BACKSEAT);
+        $msg_id = $this->makeMsgId($box_no, $this->userRoleTag, CmdMap::CONTROL_REMOTE_OPEN_HUB_LOCK);
         $str = $this->makeSendMsg(CmdMap::CONTROL_REMOTE_OPEN_HUB_LOCK, $msg_id);
         return $this->send($box_no, $str, $isSync, $msg_id);
     }
@@ -218,7 +218,7 @@ class Control implements ControlInterface
      */
     public function closeHelmet($box_no, $isSync = -1)
     {
-        $msg_id = $this->makeMsgId($box_no, $this->userRoleTag, CmdMap::CONTROL_REMOTE_CLOSE_BACKSEAT);
+        $msg_id = $this->makeMsgId($box_no, $this->userRoleTag, CmdMap::CONTROL_REMOTE_CLOSE_HUB_LOCK);
         $str = $this->makeSendMsg(CmdMap::CONTROL_REMOTE_CLOSE_HUB_LOCK, $msg_id);
         return $this->send($box_no, $str, $isSync, $msg_id);
     }
